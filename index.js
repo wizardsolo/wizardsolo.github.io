ffd3618f41c7d8f87c786cdd4c06a0032335a134
qatu <roman@rxman.my>
2026-05-12 21:47:07 -0400

    yayy

---
+++ index.js
@@ -0,0 +1,8 @@
+const enterScreen = document.getElementById('enter-screen');
+const music = new Audio('/stuff/music.mp3');
+music.loop = true;
+
+enterScreen.addEventListener('click', () => {
+  enterScreen.classList.add('hidden');
+  music.play();
+});
