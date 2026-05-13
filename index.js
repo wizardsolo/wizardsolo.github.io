36b0086e52da21ae1707e55c0e1d6f98e4b17a8d
qatual <roman@rxman.my>
2026-05-13 18:18:19 -0400

    revamppppppp

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const enterScreen = document.getElementById('enter-screen');
+const bgVideo = document.getElementById('tungtungtuffer');
 const music = new Audio('stuff/music.mp3');
 music.loop = true;
 
 enterScreen.addEventListener('click', () => {
   enterScreen.classList.add('hidden');
+  bgVideo.play();
+  bgVideo.classList.add('visible');
   music.play();
 });
